c1074a1f5df19b348a58eaae4a0cc1561d3da9bd
siddhi <siddhi@BSS-IT-DT-87>
2026-09-10 15:21:07 +0530

    New Controller added

---
+++ SimpleCICD/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCICD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello from CI test!",
+                timestamp = DateTime.UtcNow,
+                status = "success"
+            });
+        }
+    }
+}
